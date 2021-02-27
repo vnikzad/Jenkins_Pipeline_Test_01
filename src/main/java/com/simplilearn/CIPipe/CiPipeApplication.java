475f3afc6846b6f9eb39d6d61702b46f588ea3b3
student <v.nikzad@gmail.com>
2021-02-27 16:19:03 +0000

    First Commit

---
+++ src/main/java/com/simplilearn/CIPipe/CiPipeApplication.java
@@ -0,0 +1,13 @@
+package com.simplilearn.CIPipe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CiPipeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CiPipeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/simplilearn/CIPipe/CiPipeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.simplilearn.CIPipe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CiPipeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
